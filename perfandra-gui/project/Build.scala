c8fd8aae6beaf06a782bc3db2a6f0a0da3de777e
Marc CARRÉ <carre.marc@gmail.com>
2013-03-23 18:53:21 +0000

    Added support for Jasmine BDD framework using sbt-jasmine-plugin.

--- perfandra-gui/project/Build.scala
+++ perfandra-gui/project/Build.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 import play.Project._
 import com.github.play2war.plugin._
+import com.gu.SbtJasminePlugin._
 
 object ApplicationBuild extends Build {
 
@@ -15,9 +16,21 @@ object ApplicationBuild extends Build {
   )
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Compatibility with old servlet containers like Apache Tomcat 6:
-    Play2WarKeys.servletVersion := "2.5"
-  ).settings(Play2WarPlugin.play2WarSettings: _*)
-
+  val main = play.Project(appName, appVersion, appDependencies)
+    .settings(Play2WarPlugin.play2WarSettings: _*)
+    .settings(jasmineSettings : _*)
+    .settings(
+      // Compatibility with old servlet containers (e.g. Apache Tomcat 6):
+      Play2WarKeys.servletVersion := "2.5",
+        
+      // Jasmine configuration, overridden as we don't follow the default project structure sbt-jasmine expects
+      appJsDir <+= baseDirectory / "app/assets/javascripts",
+      appJsLibDir <+= baseDirectory / "public/javascripts",
+      jasmineTestDir <+= baseDirectory / "test/assets/javascripts",
+      jasmineConfFile <+= baseDirectory / "test/assets/javascripts/jasmineConfFile.js",
+      
+      // Link Jasmine to the standard 'sbt test' action. 
+      // When running 'test' Jasmine tests will be run and after that other Play tests will be executed.
+      (test in Test) <<= (test in Test) dependsOn (jasmine)        
+  )
 }
---
+++ perfandra-gui/test/assets/javascripts/jasmineConfFile.js
@@ -0,0 +1,9 @@
+// Examples:
+// EnvJasmine.loadGlobal(EnvJasmine.libDir + "*.js");
+// EnvJasmine.loadGlobal(EnvJasmine.testDir + "*.js");
+// EnvJasmine.loadGlobal(EnvJasmine.rootDir + "*.js");
+
+// Dependencies for unit testing using Jasmine:
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery-1.9.0.min.js");
+
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "example.js");
