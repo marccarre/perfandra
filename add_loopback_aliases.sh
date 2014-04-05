2a3d841bff1563947f8aa06fc30ae79340b2730c
Marc CARRE <carre.marc+github@gmail.com>
2014-04-05 07:25:20 +0100

    Added script for loopback aliases. Required by cassandra-maven-plugin under Mac OS X.

---
+++ add_loopback_aliases.sh
@@ -0,0 +1,8 @@
+# Under Mac OS X only:
+ifconfig lo0 alias 127.0.0.2
+ifconfig lo0 alias 127.0.0.3
+ifconfig lo0 alias 127.0.0.4
+
+# See also:
+# http://mojo.codehaus.org/cassandra-maven-plugin/examples/integration-tests.html
+
